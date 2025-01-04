32440e5653790b4ed9159f89e8f1ef8e9574026c
Ftnn <fufi.fr@yandex.ru>
2025-01-04 10:47:20 +0400

    base64 img(на всякий)

--- extras/img to base64.py
+++ extras/img to base64.py
@@ -11,23 +11,20 @@ with open(json_file_path, "r", encoding="utf-8") as file:
 
 # Преобразование изображений в Base64
 for entry in data:
-    bot_info = entry.get("Бот")
-    # Проверяем, является ли bot_info словарём
-    if isinstance(bot_info, dict):
-        image_path = bot_info.get("image_path")
-        
-        if image_path and Path(image_path.strip()).exists():
-            with open(image_path.strip(), "rb") as img_file:
-                base64_string = base64.b64encode(img_file.read()).decode("utf-8")
-                bot_info["base64_image"] = base64_string
-        else:
-            bot_info["base64_image"] = None
-    else:
-        entry["base64_image"] = None  # Добавляем поле для записей, где "Бот" не является словарём
+    for key, value in entry.items():
+        if isinstance(value, dict) and "image_path" in value:
+            image_path = value["image_path"].strip()
+            if Path(image_path).exists():
+                with open(image_path, "rb") as img_file:
+                    base64_string = base64.b64encode(img_file.read()).decode("utf-8")
+                    value["base64_image"] = base64_string
+            else:
+                value["base64_image"] = None
+                print(image_path)
 
 # Сохранение данных обратно в JSON
 output_file_path = "extras/main copy.json"
 with open(output_file_path, "w", encoding="utf-8") as file:
-    json.dump(data, file, ensure_ascii=False, indent=4)
+    json.dump(data, file, ensure_ascii=False, indent=2)
 
 print(f"Файл сохранен с Base64: {output_file_path}")
